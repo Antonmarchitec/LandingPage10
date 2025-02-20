4bec9723c6a4d3177e66bc2cdd28009fe9746280
Antonio Ponce <desarrollowebaps@gmail.com>
2025-02-20 01:38:10 -0400

    17_desarrolloConcecionario

--- JS/main.js
+++ JS/main.js
@@ -15,18 +15,40 @@ const funcionPrincipal = async () =>{
         const res = await fetch("api.json")
         const data = await res.json()
         //console.log(data)
-        funcionSecundaria(data)
+        funcionTargetRoja(data)
+
 
         document.addEventListener("click", (e)=>{
             if(e.target.id === "num1"){
                 console.log("Presionaste el num1")
+                contTargetCard.textContent = funcionTargetRoja(data)
+                funcionTargetRoja(data)
+
+                let numeroUno = document.querySelector("#num1")
+                numeroUno.style.background = "#f90000"
+                numeroUno.style.transition = "0.5s all"
+                numeroUno.style.color = "white"
+
+
 
             }else if(e.target.id === "num2"){
                 console.log("Presionaste el num2")
+                contTargetCard.textContent = funcionTargetAmarilla(data)
+                funcionTargetAmarilla(data)
+
+                let numeroDos = document.querySelector("#num2")
+                numeroDos.style.background = "#ffef11"
+                numeroDos.style.transition = "0.5s all"
+                numeroDos.style.color = "white"
             }else if(e.target.id === "num3"){
                 console.log("Presionaste el num3")
-            }else if(e.target.id === "num4"){
-                console.log("Presionaste el num4")
+                contTargetCard.textContent = funcionTargetVerde(data)
+                funcionTargetVerde(data)
+
+                let numeroTres = document.querySelector("#num3")
+                numeroTres.style.background = "#1e9104"
+                numeroTres.style.transition = "0.5s all"
+                numeroTres.style.color = "white"
             }
         })
 
@@ -36,43 +58,51 @@ const funcionPrincipal = async () =>{
 }
 
 
-const funcionSecundaria = (data) =>{
-    //console.log(data)
-    data.forEach( element => {
-        console.log(element)
-        
-        contTemplate.querySelector("#imgCar").setAttribute("src", element.imagen)
-        contTemplate.querySelector("#imgCar").setAttribute("alt", element.title)
-        contTemplate.querySelector("#titleCar").textContent = element.title
-        contTemplate.querySelector("#numCar").textContent = element.placa
-        contTemplate.querySelector("#btnCar").dataset.idBtn = element.id 
-        contTemplate.querySelector("#blockCar").style.background = element.background
-        contTemplate.querySelector("#blockCar").style.boxShadow = element.border
+const funcionTargetRoja = (data) =>{
+    //console.log(data[0].title)
+    contTemplate.querySelector("#imgCar").setAttribute("src", data[0].imagen)
+    contTemplate.querySelector("#imgCar").setAttribute("alt", data[0].title)
+    contTemplate.querySelector("#blockCar").style.background = data[0].background
 
-0
+    contTemplate.querySelector("#titleCar").textContent = data[0].title
+    contTemplate.querySelector("#numCar").textContent = data[0].placa
+    contTemplate.querySelector("#btnCar").dataset.id = data[0].id
 
-        let templateClone = contTemplate.cloneNode(true)
-        contFragment.appendChild(templateClone)
-    });
-
+    const TargetAA = contTemplate.cloneNode(true)
+    contFragment.appendChild(TargetAA)
     contTargetCard.appendChild(contFragment)
 
+}
 
+const funcionTargetAmarilla = (data) =>{
+    //console.log(data[0].title)
+    contTemplate.querySelector("#imgCar").setAttribute("src", data[1].imagen)
+    contTemplate.querySelector("#imgCar").setAttribute("alt", data[1].title)
+    contTemplate.querySelector("#blockCar").style.background = data[1].background
 
-    document.addEventListener("click", e =>{
-        if(e.target.dataset.idBtn === "1"){
-            console.log("Presionaste el boton uno")    
+    contTemplate.querySelector("#titleCar").textContent = data[1].title
+    contTemplate.querySelector("#numCar").textContent = data[1].placa
+    contTemplate.querySelector("#btnCar").dataset.id = data[1].id
 
-        }else if(e.target.dataset.idBtn === "2"){
-            console.log("Presionaste el boton dos")
-            
-        }else if(e.target.dataset.idBtn === "3"){
-            console.log("Presionaste el boton tres")
-        }
-    } )
+    const TargetAA = contTemplate.cloneNode(true)
+    contFragment.appendChild(TargetAA)
+    contTargetCard.appendChild(contFragment)
 
+}
 
+const funcionTargetVerde = (data) =>{
+    //console.log(data[0].title)
+    contTemplate.querySelector("#imgCar").setAttribute("src", data[2].imagen)
+    contTemplate.querySelector("#imgCar").setAttribute("alt", data[2].title)
+    contTemplate.querySelector("#blockCar").style.background = data[2].background
 
+    contTemplate.querySelector("#titleCar").textContent = data[2].title
+    contTemplate.querySelector("#numCar").textContent = data[2].placa
+    contTemplate.querySelector("#btnCar").dataset.id = data[2].id
+
+    const TargetAA = contTemplate.cloneNode(true)
+    contFragment.appendChild(TargetAA)
+    contTargetCard.appendChild(contFragment)
 
 }
 
